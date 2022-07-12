fb78b4609e52ecbc79e12a7a5b3628309a7b02ec
chioge <38528801+chioge@users.noreply.github.com>
2022-07-12 11:42:48 +0200

    Update datamodule_factory.py

--- datamodule_factory.py
+++ datamodule_factory.py
@@ -5,12 +5,20 @@ Create dataloaders depending on settings in config.py
 from config import config
 from src.models.hyperparameters import params
 from src.data.FIRST.first_datamodule import FIRST_DataModule
+from src.data.PARITY.parity_datamodule import PARITY_DataModule
 from pathlib import Path
 
 
 def get_datamodule():
     if config['dataset'] == 'first':
         return FIRST_DataModule(
+            word_length=params[config['model']]['word_length'],
+            len = params[config['model']]['len'],
+            leq = params[config['model']]['leq'],
+            batch_size=params[config['model']]['batch_size']
+            )
+    elif config['dataset'] == 'parity':
+        return PARITY_DataModule(
             word_length=params[config['model']]['word_length'],
             len = params[config['model']]['len'],
             leq = params[config['model']]['leq'],
